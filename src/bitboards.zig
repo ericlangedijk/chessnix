34cfac90d39daca5d550ef23af153c1ea3b88f9b
eric <nomail>
2025-07-14 12:50:21 +0200

    bitboards

---
+++ src/bitboards.zig
@@ -0,0 +1,44 @@
+//! constant bitboard values.
+
+pub const bb_rank_1: u64 = 0x00000000000000ff; // a1 - h1
+pub const bb_rank_2: u64 = 0x000000000000ff00;
+pub const bb_rank_3: u64 = 0x0000000000ff0000;
+pub const bb_rank_4: u64 = 0x00000000ff000000;
+pub const bb_rank_5: u64 = 0x000000ff00000000;
+pub const bb_rank_6: u64 = 0x0000ff0000000000;
+pub const bb_rank_7: u64 = 0x00ff000000000000;
+pub const bb_rank_8: u64 = 0xff00000000000000;
+
+pub const bb_file_a: u64 = 0x0101010101010101; // a1 - a8
+pub const bb_file_b: u64 = 0x0202020202020202;
+pub const bb_file_c: u64 = 0x0404040404040404;
+pub const bb_file_d: u64 = 0x0808080808080808;
+pub const bb_file_e: u64 = 0x1010101010101010;
+pub const bb_file_f: u64 = 0x2020202020202020;
+pub const bb_file_g: u64 = 0x4040404040404040;
+pub const bb_file_h: u64 = 0x8080808080808080;
+
+pub const bb_borders = bb_rank_1 | bb_rank_8 | bb_file_a | bb_file_h;
+
+// rank and file indexes
+pub const rank_1 : u3 = 0;
+pub const rank_2 : u3 = 1;
+pub const rank_3 : u3 = 2;
+pub const rank_4 : u3 = 3;
+pub const rank_5 : u3 = 4;
+pub const rank_6 : u3 = 5;
+pub const rank_7 : u3 = 6;
+pub const rank_8 : u3 = 7;
+
+pub const file_a : u3 = 0;
+pub const file_b : u3 = 1;
+pub const file_c : u3 = 2;
+pub const file_d : u3 = 3;
+pub const file_e : u3 = 4;
+pub const file_f : u3 = 5;
+pub const file_g : u3 = 6;
+pub const file_h : u3 = 7;
+
+pub const bb_full: u64 = 0xffffffffffffffff;
+pub const bb_black_squares: u64 = 0b01010101_10101010_10101010_01010101_10101010_01010101_10101010_01010101;
+pub const bb_white_squares: u64 = ~bb_black_squares;
