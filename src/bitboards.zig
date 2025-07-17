f27c156fc9efb8038c20650ef01624786805836d
eric <nomail>
2025-07-17 22:59:01 +0200

    wip

--- src/bitboards.zig
+++ src/bitboards.zig
@@ -1,6 +1,6 @@
-//! constant bitboard values.
+//! Constant bitboard values.
 
-pub const bb_rank_1: u64 = 0x00000000000000ff; // a1 - h1
+pub const bb_rank_1: u64 = 0x00000000000000ff;
 pub const bb_rank_2: u64 = 0x000000000000ff00;
 pub const bb_rank_3: u64 = 0x0000000000ff0000;
 pub const bb_rank_4: u64 = 0x00000000ff000000;
@@ -9,7 +9,7 @@ pub const bb_rank_6: u64 = 0x0000ff0000000000;
 pub const bb_rank_7: u64 = 0x00ff000000000000;
 pub const bb_rank_8: u64 = 0xff00000000000000;
 
-pub const bb_file_a: u64 = 0x0101010101010101; // a1 - a8
+pub const bb_file_a: u64 = 0x0101010101010101;
 pub const bb_file_b: u64 = 0x0202020202020202;
 pub const bb_file_c: u64 = 0x0404040404040404;
 pub const bb_file_d: u64 = 0x0808080808080808;
@@ -19,6 +19,12 @@ pub const bb_file_g: u64 = 0x4040404040404040;
 pub const bb_file_h: u64 = 0x8080808080808080;
 
 pub const bb_borders = bb_rank_1 | bb_rank_8 | bb_file_a | bb_file_h;
+pub const bb_full: u64 = 0xffffffffffffffff;
+pub const bb_black_squares: u64 = 0b01010101_10101010_10101010_01010101_10101010_01010101_10101010_01010101;
+pub const bb_white_squares: u64 = ~bb_black_squares;
+
+pub const rank_bitboards: [8]u64 = .{ bb_rank_1, bb_rank_2, bb_rank_3, bb_rank_4, bb_rank_5, bb_rank_6, bb_rank_7, bb_rank_8 };
+pub const file_bitboards: [8]u64 = .{ bb_file_a, bb_file_b, bb_file_c, bb_file_d, bb_file_e, bb_file_f, bb_file_g, bb_file_h };
 
 // rank and file indexes
 pub const rank_1 : u3 = 0;
@@ -39,6 +45,5 @@ pub const file_f : u3 = 5;
 pub const file_g : u3 = 6;
 pub const file_h : u3 = 7;
 
-pub const bb_full: u64 = 0xffffffffffffffff;
-pub const bb_black_squares: u64 = 0b01010101_10101010_10101010_01010101_10101010_01010101_10101010_01010101;
-pub const bb_white_squares: u64 = ~bb_black_squares;
+pub const ranks: [8]u3 = .{ rank_1, rank_2, rank_3, rank_4, rank_5, rank_6, rank_7, rank_8 };
+pub const files: [8]u3 = .{ file_a, file_b, file_c, file_d, file_e, file_f, file_g, file_h };
