f255fa894ed4d1d0cdcb77a15a32eed375b641af
eric <nomail>
2025-09-03 20:49:05 +0200

    formatting

--- src/uci.zig
+++ src/uci.zig
@@ -24,11 +24,9 @@ const io = lib.io;
 
 const eql = funcs.eql;
 
-pub fn run() void
-{
+pub fn run() void {
     // TODO: what to do in non-terminal mode? Just crash?
-    uci_loop() catch |err|
-    {
+    uci_loop() catch |err| {
         lib.io.debugprint("ERROR: {s}.\n\nPress any key to quit.\n", .{ @errorName(err) });
         //_ = lib.in.readByte() catch {}; TODO: repair 0.15.1
     };
@@ -41,20 +39,17 @@ fn uci_loop() !void
 
     const is_tty = lib.is_tty();
 
-    if (is_tty)
-    {
+    if (is_tty) {
         try io.print("chessnix {s} by eric\n", .{ lib.version });
     }
 
-    command_loop: while (true)
-    {
+    command_loop: while (true) {
         const input = try io.readline() orelse break;
         var tokenizer: Tokenizer = std.mem.tokenizeScalar(u8, input, ' ');
         const cmd: []const u8 = tokenizer.next() orelse continue :command_loop;
 
         // Uci commands.
-        if (eql(cmd, "uci"))
-        {
+        if (eql(cmd, "uci")) {
             try io.print
             (
                 \\id chessnix {s}
@@ -64,85 +59,68 @@ fn uci_loop() !void
                 , .{ lib.version }
             );
         }
-        else if (eql(cmd, "isready"))
-        {
+        else if (eql(cmd, "isready")) {
             try io.print("readyok\n", .{});
         }
-        else if (eql(cmd, "ucinewgame"))
-        {
+        else if (eql(cmd, "ucinewgame")) {
             try engine.set_startpos(null);
         }
-        else if (eql(cmd, "go"))
-        {
+        else if (eql(cmd, "go")) {
             const go: Go = parse_go(&tokenizer) catch continue :command_loop;
             try engine.start(&go);
             // Each go command must be eventually responded to with bestmove, once the search is completed or interrupted with stop.
         }
-        else if (eql(cmd, "stop"))
-        {
+        else if (eql(cmd, "stop")) {
             try engine.stop();
             // Return bestmove if we were thinking, otherwise ignore.
         }
-        else if (eql(cmd, "position"))
-        {
+        else if (eql(cmd, "position")) {
             parse_position(&tokenizer) catch |err| { print_error(err); continue :command_loop; };
         }
-        else if (eql(cmd, "quit"))
-        {
+        else if (eql(cmd, "quit")) {
             return;
         }
         // Custom commands in terminal.
-        else if (is_tty)
-        {
-            if (eql(cmd, "d"))
-            {
+        else if (is_tty) {
+            if (eql(cmd, "d")) {
                 try engine.pos.draw();
             }
-            else if (eql(cmd, "bench"))
-            {
+            else if (eql(cmd, "bench")) {
                 try perft.bench();
             }
-            else if (eql(cmd, "perft"))
-            {
+            else if (eql(cmd, "perft")) {
                 const next = tokenizer.next() orelse continue :command_loop;
                 const depth: u8 = std.fmt.parseInt(u8, next, 10) catch continue :command_loop;
                 perft.run(&engine.pos, depth);
             }
-            else if (eql(cmd, "qperft"))
-            {
+            else if (eql(cmd, "qperft")) {
                 const next = tokenizer.next() orelse continue :command_loop;
                 const depth: u8 = std.fmt.parseInt(u8, next, 10) catch continue :command_loop;
                 perft.qrun(&engine.pos, depth);
             }
             // DEBUG TEMP
-            else if (eql(cmd, "kiwi"))
-            {
+            else if (eql(cmd, "kiwi")) {
                 try engine.set_position(tests.kiwi_fen, null);
             }
-            else if (eql(cmd, "deb"))
-            {
+            else if (eql(cmd, "deb")) {
                 try temp();
             }
             // DEBUG TEMP
-            else if (eql(cmd, "c"))
-            {
+            else if (eql(cmd, "c")) {
                 const next = tokenizer.next() orelse continue :command_loop;
                 const depth: u8 = std.fmt.parseInt(u8, next, 10) catch continue :command_loop;
                 perft.run_captures(&engine.pos, depth);
             }
             // DEBUG TEMP
-            else if (eql(cmd, "r"))
-            {
+            else if (eql(cmd, "r")) {
                 _ = engine.pos.is_threefold_repetition();
             }
             // DEBUG TEMP
-            else if (eql(cmd, "m"))
-            {
+            else if (eql(cmd, "m")) {
                 try engine.pos.print_history();
             }
             // DEBUG TEMP
-            else if (eql(cmd, "e"))
-            {
+            else if (eql(cmd, "e")) {
                 const e = eval.evaluate_abs(&engine.pos, true);
                 try io.print("eval abs = {}\n", .{ e });
                 //eval.bench(&engine.pos);
@@ -152,28 +130,22 @@ fn uci_loop() !void
 }
 
 /// TODO: Not sure yet how and what handling errors in ucimode / terminal mode.
-pub fn print_error(err: anyerror) void
-{
+pub fn print_error(err: anyerror) void {
     lib.io.print("info string error: {t}\n", .{ err }) catch lib.wtf();
 }
 
 /// Parce uci command after "position"
-fn parse_position(tokenizer: *Tokenizer) !void
-{
+fn parse_position(tokenizer: *Tokenizer) !void {
     const next = tokenizer.next() orelse return;
-    if (eql(next, "fen"))
-    {
+    if (eql(next, "fen")) {
         var fen_and_moves = std.mem.splitSequence(u8, tokenizer.rest(), "moves");
         try engine.set_position(fen_and_moves.next(), fen_and_moves.next());
     }
-    else if (eql(next, "startpos"))
-    {
-        if (tokenizer.next()) |n|
-        {
+    else if (eql(next, "startpos")) {
+        if (tokenizer.next()) |n| {
             if (eql(n, "moves")) try engine.set_startpos(tokenizer.rest());
         }
-        else
-        {
+        else {
             try engine.set_startpos(null);
         }
     }
@@ -184,58 +156,44 @@ fn parse_go(tokenizer: *Tokenizer) !Go
 {
     var go: Go = .empty;
 
-    while (tokenizer.next()) |next|
-    {
-        if (eql(next, "ponder"))
-        {
+    while (tokenizer.next()) |next| {
+        if (eql(next, "ponder")) {
             go.ponder = true;
         }
-        else if (eql(next, "wtime"))
-        {
+        else if (eql(next, "wtime")) {
             go.wtime = parse_nr(tokenizer.next()) orelse return Error.ParsingError;
         }
-        else if (eql(next, "btime"))
-        {
+        else if (eql(next, "btime")) {
             go.btime = parse_nr(tokenizer.next()) orelse return Error.ParsingError;
         }
-        else if (eql(next, "winc"))
-        {
+        else if (eql(next, "winc")) {
             go.winc = parse_nr(tokenizer.next()) orelse return Error.ParsingError;
         }
-        else if (eql(next, "binc"))
-        {
+        else if (eql(next, "binc")) {
             go.binc = parse_nr(tokenizer.next()) orelse return Error.ParsingError;
         }
-        else if (eql(next, "movestogo"))
-        {
+        else if (eql(next, "movestogo")) {
             go.movestogo = parse_nr(tokenizer.next()) orelse return Error.ParsingError;
         }
-        else if (eql(next, "movetime"))
-        {
+        else if (eql(next, "movetime")) {
             go.movetime = parse_nr(tokenizer.next()) orelse return Error.ParsingError;
         }
-        else if (eql(next, "depth"))
-        {
+        else if (eql(next, "depth")) {
             go.depth = parse_nr(tokenizer.next()) orelse return Error.ParsingError;
         }
-        else if (eql(next, "infinite"))
-        {
+        else if (eql(next, "infinite")) {
             go.infinite = true;
         }
    }
    return go;
 }
 
-fn parse_nr(str: ?[]const u8) ?u64
-{
+fn parse_nr(str: ?[]const u8) ?u64 {
     if (str == null) return null;
     return std.fmt.parseInt(u64, str.?, 10) catch null;
 }
 
-pub const Go = struct
-{
-    const empty: Go = .{};
-
+pub const Go = struct {
     /// Not used (yet).
     ponder: ?bool = null,
     /// Not used (yet).
@@ -256,17 +214,17 @@ pub const Go = struct
     movetime: ?u64 = null,
     /// Infinite search. Overwrites the other limiting fields.
     infinite: ?bool = null,
+
+    const empty: Go = .{};
 };
 
 /// Error parsing UCI string.
-const Error = error
-{
+const Error = error {
     ParsingError,
 };
 
 /// Just some stupid debug function.
-fn temp() !void
-{
+fn temp() !void {
 
     try io.print("{f}\n", .{&engine.pos});
     if (true) return;
